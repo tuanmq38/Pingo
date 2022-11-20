61b3efe4d06d0812a55f0f403e53f315e0b56374
Quintus Mai <65383374+tuanmq38@users.noreply.github.com>
2022-11-19 17:15:44 -0800

    Authentication completed

--- src/App.js
+++ src/App.js
@@ -6,14 +6,49 @@ import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import Sidebar from "./components/Sidebar";
 import Rightbar from "./components/Rightbar";
+import { useState } from "react";
+import { ConnectButton } from "web3uikit";
 
 const App = () => {
 
+  const [walletAddress, setWalletAddress] = useState("");
+
+  async function requestAccount() {
+    // Check if MetaMask extension exits
+    if(window.ethereum) {
+      console.log("MetaMask is available!");
+
+      try {
+        const accounts = await  window.ethereum.request({
+          method: "eth_requestAccounts"
+        });
+        setWalletAddress(accounts[0]);
+        console.log(accounts)
+      } catch (err) {
+        console.log(err);
+      }
+
+    } else {
+      alert('MetaMask extension need to be installed');
+    }
+  }
+
+  // Create a provider to interact with a smart contract
+  
+
   return (
     <>
+    {walletAddress ? (
       <div className="page">
         <div className="sideBar">
             <Sidebar/>
+            <div className="logout"
+            onClick={() => {
+              window.location.reload();
+            }}
+            >
+              Logout
+            </div>
           </div>
         
           <div className="mainWindow">
@@ -27,6 +62,11 @@ const App = () => {
           <Rightbar/>
         </div>
       </div>
+    ) : (
+      <div className="loginPage">
+        <button onClick={requestAccount}>Connect</button>
+      </div>
+    )}
     </>
   );
 };
